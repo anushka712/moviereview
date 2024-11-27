5b5fcc00229b1089395cd59c927309f5f2f9494e
anushka aryal <aryalanushka2020@gmail.com>
2024-11-27 16:41:43 +0545

    project initialization

---
+++ src/app/new/page.tsx
@@ -0,0 +1,7 @@
+export default function New(){
+    return(
+        <div>
+            <h1>This is a new page</h1>
+        </div>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+    <h1>Start of next.js</h1>
+   </div>
+  );
+}
